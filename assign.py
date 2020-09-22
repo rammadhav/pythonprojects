def9a298afe45c1b903c4e9f2d8cdc07e9bed689
rammadhav <madhav9th@gmail.com>
2020-09-22 20:41:30 +0530

    modified code

--- assign.py
+++ assign.py
@@ -2,25 +2,21 @@ import json
 
 with open('interface-data.json') as f:
     data = json.load(f)
-# content = data['imdata']
-# print(type(content))
-# for i in content:
-#     access = i['l1PhysIf'] ['attributes']
-#     print(access)
-#     name = access['dn'],access['speed'],access['mtu']
+
 def get_result():
     content = data['imdata']
     for i in content:
-        access = i['l1PhysIf']['attributes']
-        name = access['dn'],access['speed'],access['mtu']
-        print(name)
+        dn = i['l1PhysIf']['attributes']['dn']
+        speed = i['l1PhysIf']['attributes']['speed']
+        mtu = i['l1PhysIf']['attributes']['mtu']
+        print(dn, speed, mtu)
 def get_content():
     content = data['imdata']
     for i in content:
         access = i['l1PhysIf']['attributes']
         if access['switchingSt'] == 'disabled' and access['layer'] == 'Layer3':
             print(access['dn'])
-print()
+            
 def get_leaf():
     content = data['imdata']
     for i in content:
