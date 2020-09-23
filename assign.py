a4cfea0cfb1a99e260414a2230c476978518c259
rammadhav <madhav9th@gmail.com>
2020-09-23 11:05:30 +0530

    using lamda func

--- assign.py
+++ assign.py
@@ -1,31 +1,42 @@
 import json
-
+from functools import  reduce
 with open('interface-data.json') as f:
     data = json.load(f)
 def get_result():
     content = data['imdata']
-    for i in content:
-        dn = i['l1PhysIf']['attributes']['dn']
-        speed = i['l1PhysIf']['attributes']['speed']
-        mtu = i['l1PhysIf']['attributes']['mtu']
+    name = list(map(lambda x : x['l1PhysIf']['attributes'],content))
+    for i in name:
+        dn = i['dn']
+        speed = i['speed']
+        mtu = ['mtu']
         print(dn, speed, mtu)
 def get_content():
     content = data['imdata']
-    for i in content:
-        access = i['l1PhysIf']['attributes']
-        if access['switchingSt'] == 'disabled' and access['layer'] == 'Layer3':
-            print(access['dn'])
+    name = list(map(lambda x: x['l1PhysIf']['attributes'], content))
+    for i in name:
+        if i['switchingSt'] == 'disabled' and i['layer'] == 'Layer3':
+            print(i['dn'])
             
 def get_leaf():
     content = data['imdata']
-    for i in content:
-        access = i['l1PhysIf']['attributes']
-        if access['portT'] == 'leaf':
-            print(access)
-    print("count =",len(access))
+    count = 0
+    name = list(map(lambda x : x['l1PhysIf']['attributes'],content))
+    for i in name:
+        if i['portT'] == 'leaf':
+            count +=1
+    print("Number of objects: ",count)
+
 def get_max():
-    lst = [10, 20, 4, 45, 99, 54, 1, 4, 15]
-    lst.sort()
+    lst = [10, 20, 4, 45, 99, 104, 1, 4, 15]
+    n = len(lst)
+    for i in range (n):
+        maxnum = i
+        for j in range (i,n):
+            if lst[j] < lst[maxnum]:
+                maxnum =j
+        temp = lst[i]
+        lst[i] = lst[maxnum]
+        lst[maxnum] = temp
     print("The max integer of the list:",lst[-1])
 
 get_result()
